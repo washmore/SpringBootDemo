dbe11fc44ac741a6a2de90d98e86a821f74365a2
Washmore <1102468869@qq.com>
2017-06-25 17:32:34 +0800

    打包方式显示指定为jar

---
+++ src/main/java/tech/washmore/demo/springboot/themeleaf/ThemeLeafDemoController.java
@@ -0,0 +1,23 @@
+package tech.washmore.demo.springboot.themeleaf;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author Washmore
+ * @version V1.0
+ * @summary themeleafDemo
+ * @Copyright (c) 2017, Lianjia Group All Rights Reserved.
+ * @since 2017/6/25
+ */
+@Controller
+public class ThemeLeafDemoController {
+    @RequestMapping("/themeleaf")
+    public ModelAndView themeleafDemo(ModelAndView mav) {
+        mav.addObject("hello", "hello");
+        mav.addObject("world", "world");
+        mav.setViewName("themleafdemo");
+        return mav;
+    }
+}
