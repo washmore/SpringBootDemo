7dfb50ab9c4eb8cd7cce65818d8ce3993dc12c8a
Washmore <1102568869@qq.com>
2017-07-08 01:02:49 +0800

    集成mybaits

--- src/main/java/tech/washmore/demo/springboot/blog/common/config/DataSourceConfig.java
+++ src/main/java/tech/washmore/demo/springboot/blog/common/config/DataSourceConfig.java
@@ -1,11 +1,8 @@
 package tech.washmore.demo.springboot.blog.common.config;
 
 import org.mybatis.spring.SqlSessionFactoryBean;
-import org.springframework.beans.BeansException;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
