8b0149e03e39b7f9cea2feaedba0a99397f9d296
Washmore <1102568869@qq.com>
2017-07-08 20:52:55 +0800

    6KW数据测试
    washmore`

--- src/test/java/tech/washmore/demo/springboot/mongodb/TestMongDbCURD.java
+++ src/test/java/tech/washmore/demo/springboot/mongodb/TestMongDbCURD.java
@@ -11,6 +11,7 @@ import tech.washmore.demo.springboot.mongodb.repository.MongdbArticle;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by Washmore on 2017/7/8.
@@ -24,17 +25,31 @@ public class TestMongDbCURD extends BaseTest {
 
     @Test
     public void addData() {
-        MongdbArticle mongdbArticle1 = new MongdbArticle(null, "文章标题1", "我们都是中国人233333");
-        MongdbArticle mongdbArticle2 = new MongdbArticle(null, "文章标题11", "我们都是地球人233333");
-        MongdbArticle mongdbArticle3 = new MongdbArticle(null, "文章标题111", "我们都是亚洲人233333");
-        MongdbArticle mongdbArticle4 = new MongdbArticle(null, "文章标题1111", "我们不是日本人233333");
-        MongdbArticle mongdbArticle5 = new MongdbArticle(null, "文章标题11111", "不转不是中国人233333");
-        MongdbArticle mongdbArticle6 = new MongdbArticle(null, "文章标题111111", "都是些什么人233333");
-        articleRepository.save(mongdbArticle1);
-        articleRepository.save(mongdbArticle2);
-        articleRepository.save(mongdbArticle3);
-        articleRepository.save(Arrays.asList(mongdbArticle4, mongdbArticle5, mongdbArticle6));
+        for (int i = 275237; i < 10000000; i += 200000) {
+            final int start = i;
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int s = start; s < start + 200000; s++) {
+                        MongdbArticle mongdbArticle1 = new MongdbArticle(null, "文章标题1" + s, "我们都是中国人233333" + s);
+                        MongdbArticle mongdbArticle2 = new MongdbArticle(null, "文章标题11" + s, "我们都是地球人233333" + s);
+                        MongdbArticle mongdbArticle3 = new MongdbArticle(null, "文章标题111" + s, "我们都是亚洲人233333" + s);
+                        MongdbArticle mongdbArticle4 = new MongdbArticle(null, "文章标题1111" + s, "我们不是日本人233333" + s);
+                        MongdbArticle mongdbArticle5 = new MongdbArticle(null, "文章标题11111" + s, "不转不是中国人233333" + s);
+                        MongdbArticle mongdbArticle6 = new MongdbArticle(null, "文章标题111111" + s, "都是些什么人233333" + s);
+                        articleRepository.save(mongdbArticle1);
+                        articleRepository.save(mongdbArticle2);
+                        articleRepository.save(mongdbArticle3);
+                        articleRepository.save(Arrays.asList(mongdbArticle4, mongdbArticle5, mongdbArticle6));
+                        System.out.println(Thread.currentThread().getName() + ":" + s);
+                    }
+                }
+            }).start();
+        }
+
+        new Scanner(System.in).next();
     }
+
 
     @Test
     public void testFindByTitle() {
